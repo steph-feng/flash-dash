17c16c8aca41bd84caab5e1b21339e8821a993cb
Stephanie Feng <stephanie.c.feng@gmail.com>
2024-01-21 08:59:52 -0800

    Added create new set modal

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,12 +1,17 @@
 'use client'
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { useEffect, useState } from 'react';
+import Modal from '@mui/material/Modal';
 
 export default function Dashboard() {
   const { user, error, isLoading } = useUser();
   const [inputText, setInputText] = useState("");
   const [fetchingFromMongo, setFetchingFromMongo] = useState(true); 
   const [dbData, setDbData] = useState([]);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   async function getFromMongoDB() {
     const response = await fetch('/api/db', {
@@ -59,8 +64,29 @@ export default function Dashboard() {
         ))
         }
         </div>
-      <textarea onChange={(e) => setInputText(e.target.value)}></textarea>
-      <button onClick={sendToGPTAndDB}>Enter</button>
+
+      <button onClick={handleOpen}>create new set</button>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        sx = {{position: "absolute", width: "50%", height: "50vh", top: "30vh", left: "25%"}}
+      >
+        <div className='bg-[#AFC0B5] flex flex-col justify-center items-start p-8 rounded-lg drop-shadow'>
+            <div>new flash card set</div>
+            <div id="title" className='flex flex-row my-5'>
+                <p className='mr-3'>title:</p>
+                <input type='text' style={{width: "40vmin"}}/>
+            </div>
+            <div id='upload' className='flex flex-col'>
+                <textarea 
+                placeholder='write or paste your notes here' 
+                className='p-2' style={{width: "45.5vw"}}
+                onChange={(e) => setInputText(e.target.value)}></textarea>
+                <button onClick={sendToGPTAndDB} className='bg-white w-1/3 my-4 rounded self-end'>convert to flash cards!</button>
+            </div>
+        </div>
+      </Modal>
       
     </div>
   );
