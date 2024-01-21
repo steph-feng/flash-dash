5dbc3666f3765f4cc153d07d56d7cb2ce5e8e049
karenzhao35 <karenzhao35@gmail.com>
2024-01-21 09:39:29 -0800

    fixed Card call

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -56,7 +56,7 @@ export default function Dashboard() {
         <main className="flex min-h-screen flex-col items-center justify-between p-24" style={containerStyle}>
             <User/>
 
-            <Cards/>
+            
         </main>
       );
 }
