f8ad7bd81e88d1729a6273f44f7897fc9e39a4ba
Stephanie Feng <stephanie.c.feng@gmail.com>
2024-01-21 10:08:09 -0800

    initial

---
+++ app/flashcards/page.js
@@ -0,0 +1,28 @@
+
+
+//import React, { useState} from "react";
+import Link from 'next/link'
+
+import Deck from "./Deck";
+import populate from "./AICall.js"
+import "./cards.css";
+
+export default async function Flashcards() {
+
+    let questions = await populate()
+    let listofQuestions = JSON.parse(questions).flashcards
+    console.log(listofQuestions)
+
+
+    return (
+
+        <div class="card-display">
+
+            <h1>Flash Cards</h1>
+
+            <Deck questions={listofQuestions} />
+
+            <Link href="/">Home</Link>
+        </div>
+    );
+}
