2ffeb4241a3ac54b0cfae9a1190125d666bf1ac7
Stephanie Feng <stephanie.c.feng@gmail.com>
2024-01-21 09:56:54 -0800

    Added modal

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,62 +1,106 @@
 'use client'
+
 import BackgroundImage from './dashboard.png'
 import User from './user.js'
 import Cards from './cards.js'
-import "@fontsource/kumbh-sans"; 
+import "@fontsource/kumbh-sans";
 import { useUser } from '@auth0/nextjs-auth0/client';
-// import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import Modal from '@mui/material/Modal';
 
 export default function Dashboard() {
     const { user, error, isLoading } = useUser();
 
-    async function saveToDB() {
-        await fetch('/api/db', 
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+    const [title, setTitle] = useState("");
+    const [inputText, setInputText] = useState("");
 
-            body: JSON.stringify({ data: user.email })
-        }
-        );
-    };
+    const [fetchingFromMongo, setFetchingFromMongo] = useState(true);
+    const [dbData, setDbData] = useState([]);
 
-    async function sendToGPT() {
-        const response = await fetch('/api/gpt', 
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-            body: JSON.stringify({ data: inputText })
-        })
+    async function getFromMongoDB() {
+        const response = await fetch('/api/db', {
+            method: 'GET'
+        });
 
         const data = await response.json();
-
-        console.log(data.message.choices[0].message.content);
+        setDbData(data);
+        setFetchingFromMongo(false);
     }
 
-    const containerStyle = {
-        backgroundImage: `url(${BackgroundImage.src})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        width: '100%',
-        height: '100vh',
-      };
+    async function sendToGPTAndDB() {
+        const response = await fetch('/api/gpt', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ data: inputText })
+        });
+
+        const data = await response.json();
+        const cards = data.message.choices[0].message.content;
+
+        await fetch('/api/db', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ user: user.email, set: { name: title, notes: cards } })
+        });
+    }
+
+    useEffect(() => {
+        getFromMongoDB();
+    }, []);
 
 
 
-    if (isLoading) return <div>loading...</div>;
+
+    if (isLoading | fetchingFromMongo) return <div>loading...</div>;
     if (error) return <div>{error.message}</div>;
-    
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24" style={containerStyle}>
-            <User/>
+            <User />
 
-            
+            <button onClick={handleOpen}>create new set</button>
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+                sx={{ position: "absolute", width: "50%", height: "50vh", top: "30vh", left: "25%" }}
+            >
+                <div className='bg-[#AFC0B5] flex flex-col justify-center items-start p-8 rounded-lg drop-shadow'>
+                    <div>new flash card set</div>
+
+                    <div id="title" className='flex flex-row my-5'>
+                        <p className='mr-3'>title:</p>
+                        <input type='text'
+                            style={{ width: "40vmin" }}
+                            onChange={(e) => setTitle(e.target.value)} />
+                    </div>
+
+                    <div id='upload' className='flex flex-col'>
+                        <textarea
+                            placeholder='write or paste your notes here'
+                            className='p-2' style={{ width: "45.5vw" }}
+                            onChange={(e) => setInputText(e.target.value)}></textarea>
+                        <button onClick={sendToGPTAndDB} className='bg-white w-1/3 my-4 rounded self-end'>convert to flash cards!</button>
+                    </div>
+                </div>
+            </Modal>
+
         </main>
-      );
+    );
 }
+
+const containerStyle = {
+    backgroundImage: `url(${BackgroundImage.src})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    width: '100%',
+    height: '100vh',
+};
