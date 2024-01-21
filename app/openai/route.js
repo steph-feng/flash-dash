3017a87e5c0d0a7469470018133966b48e64db08
nlmanama <nlminhanh12@gmail.com>
2024-01-20 16:08:13 -0800

    initialized api call

---
+++ app/openai/route.js
@@ -0,0 +1,20 @@
+//import OpenAI from "openai";
+require("dotenv").config();
+const OpenAI = require ('openai');
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  });
+
+
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
+}
+
+main();
