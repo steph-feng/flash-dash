7810b5b3d9b2351fe0a4448581aef08b96ace5ef
Stephanie Feng <stephanie.c.feng@gmail.com>
2024-01-21 08:01:29 -0800

    Get request from dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,48 +1,67 @@
 'use client'
-
 import { useUser } from '@auth0/nextjs-auth0/client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Dashboard() {
-    const { user, error, isLoading } = useUser();
-    const [inputText, setInputText] = useState("");
+  const { user, error, isLoading } = useUser();
+  const [inputText, setInputText] = useState("");
+  const [fetchingFromMongo, setFetchingFromMongo] = useState(true); 
+  const [dbData, setDbData] = useState([]);
 
-    async function sendToGPTAndDB() {
-        const response = await fetch('/api/gpt',
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+  async function getFromMongoDB() {
+    const response = await fetch('/api/db', {
+      method: 'GET'
+    });
 
-                body: JSON.stringify({ data: inputText })
-            })
+    const data = await response.json();
+    setDbData(data);
+    setFetchingFromMongo(false);
+  }
 
-        const data = await response.json();
-        const cards = data.message.choices[0].message.content;
+  async function sendToGPTAndDB() {
+    const response = await fetch('/api/gpt', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ data: inputText })
+    });
 
-        await fetch('/api/db',
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+    const data = await response.json();
+    const cards = data.message.choices[0].message.content;
 
-                body: JSON.stringify({ user: user.email, set: cards })
-            }
-        );
-    }
+    await fetch('/api/db', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user: user.email, set: cards })
+    });
+  }
 
+  useEffect(() => {
+       getFromMongoDB();
+  }, []);
 
-    if (isLoading) return <div>loading...</div>;
-    if (error) return <div>{error.message}</div>;
+  if (isLoading || fetchingFromMongo) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
-    return (
-        <div>
-            <h2>{user.name}</h2>
-            <p>{user.email}</p>
-            <textarea onChange={(e) => setInputText(e.target.value)}></textarea>
-            <button onClick={sendToGPTAndDB}>enter</button>
+  return (
+    <div className='h-dvh flex flex-col justify-center items-center'>
+        <div id="profileInfo" className="">
+            <p>welcome {user.name.split(" ")[0]}</p>
         </div>
-    );
+
+        <div id="scrollingFlashCardSets" className='flex flex-row' > 
+        {
+        dbData.map((item, index) => (
+          <p key={index}>{item.set}</p>
+        ))
+        }
+        </div>
+      <textarea onChange={(e) => setInputText(e.target.value)}></textarea>
+      <button onClick={sendToGPTAndDB}>Enter</button>
+      
+    </div>
+  );
 }
